67bd556dee216979ed2c1334e5efeae9634d9718
souiss_y <souiss_y@916c51f7-7e62-4f47-a287-cbda59804c34>
2017-11-16 09:06:36 +0000

    ""

--- index.php
+++ index.php
@@ -44,7 +44,8 @@
         echo '</div>';
     }
     ?>
-    <canvas id="myCanvas"></canvas>
+    <canvas id="myCanvas" width="500" height="500" style="border:1px solid #000000;">
+    </canvas>
 </div>
 <body>
 </body>
