7c302422f4eaee2d5fcec6727236afc19cef4e2b
souiss_y <souiss_y@916c51f7-7e62-4f47-a287-cbda59804c34>
2017-11-16 08:26:25 +0000

    ""

--- Complex Manager/souiss_y/index.php
+++ Complex Manager/souiss_y/index.php
@@ -30,6 +30,7 @@ if (!empty($_POST['ree']) && !empty($_POST['ima'])) {
 
     $ree = $_POST['ree'];
     $ima = $_POST['ima'];
+    echo '<span>';
     verif_nbr($ree, $ima);
     conj($ree, $ima);
     mod($ree, $ima);
@@ -37,7 +38,7 @@ if (!empty($_POST['ree']) && !empty($_POST['ima'])) {
     trig($ree, $ima);
     inv($ree, $ima);
     expo($ree, $ima);
-
+    echo '</span>';
 } else {
     echo "Veuillez remplir tous les champs";
 }
